9e692ccd6cb37367ea919685e5f63e8d1b3cab2f
Mateus Antonio <mateus.antonio18@hotmail.com>
2023-10-05 23:43:11 -0300

    project finalized

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,33 @@
+export const dataFake =[
+  {
+    'id': '1',
+    'photoCover': 'https://cinepop.com.br/wp-content/uploads/2023/10/masters-of-the-air-1-696x464.jpg.webp',
+    'photoDescription': 'https://cinepop.com.br/wp-content/uploads/2023/10/masters-of-the-air-7.jpg.webp',
+    'title': 'Masters of the Air',
+    'description': "A produção foi criada pelo mesmo time de produtores de ‘Band of Brothers’ e ‘The Pacific’, que inclui Steven Spielberg, Tom Hanks e Gary Goetzman. Cary Fukugana dirige os nove episódios da obra.    Baseado no romance de Donald L. Miller e com roteiro assinado por John Orloff, a trama acompanha os membros do Bloody Hundredth, o centésimo grupo de bombas que conduziu missões perigosas através da Alemanha Nazista em nome dos países aliados – e tudo enquanto enfrentou condições hostis a 25 mil pés de altura. Callum Turner, Anthony Boyle, Nate Mann, Rafferty Law, Josiah Cross, Branden Cook, Ncuti Gatwa e outros também fazem parte do elenco."
+
+  },
+  {
+    'id': '2',
+    'title': 'A Queda da Casa de Usher',
+    'description': 'Baseado no conto homônimo de Edgar Allan Poe, a trama acompanha os irmãos Roderick (Bruce Greenwood) e Madeline Usher (Mary McDonnell) comandando um império de riqueza fruto do sucesso da Farmacêutica Fortunato – o negócio da família. Acontece que tudo passa a ruir quando os irmãos começam a ser assassinados após o surgimento de uma misteriosa mulher do passado do patriarca.',
+    'photoCover': 'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/10/05/126288596-i806355.jpeg'
+  },
+  {
+    'id': '3',
+    'title': 'Secret Invasion',
+    'description': 'Na nova série “Secret Invasion” da Marvel Studios, ambientada no atual MCU, Nick Fury descobre sobre uma invasão clandestina da Terra por uma facção de Skrulls metamorfos. Fury se junta a seus aliados, incluindo Everett Ross, Maria Hill e Skrull Talos, que construiu uma vida na Terra. Juntos, eles correm contra o tempo para impedir uma invasão Skrull iminente e salvar a humanidade. Algumas sequências ou padrões de luzes piscantes podem afetar visualizadores fotossensíveis.',
+    'photoCover': 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/BF0C58062E27D839B00869F892B675A9D46625CE9756135651E9BEB0445C656B/scale?width=1200&aspectRatio=1.78&format=jpeg'
+  },
+  {
+    'id': '4',
+    'title': 'A Freira 2 ',
+    'description': 'BA Freira 2 é o segundo capítulo da história de A Freira (2018), que faz parte do universo da franquia Invocação do Mal. No primeiro filme, após uma freira cometer suicídio em um convento na Romênia, o Vaticano envia o atormentado Padre Burke (Demián Bichir) e uma noviça, Irmã Irene (Taissa Farmiga), para investigar o ocorrido. Arriscando suas vidas, a fé e até suas almas, os dois descobrem um segredo profano no local, confrontando uma força do mal que assume a forma de uma freira demoníaca e transforma o convento em um campo de batalha espiritual. Agora, na continuação, anos após os acontecimentos do primeiro filme, um padre é assassinado e parece que o mal está se espalhando por toda a região. Novamente acompanhamos a Irmã Irene quando, após pensar ter escapado por pouco de Valak, a entidade demoníaca, ela é forçada a enfrentar o poderoso e macabro inimigo mais uma vez.',
+    'photoCover': 'https://s2-techtudo.glbimg.com/tXrHYaT9UijAZYHzxkJZ4D7rnoo=/0x0:1200x675/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/H/l/Z1VeBjQ8AUmIdWaz7JTQ/whatsapp-image-2023-06-11-at-21.19.19.jpeg'
+  }
+
+
+
+
+
+]
